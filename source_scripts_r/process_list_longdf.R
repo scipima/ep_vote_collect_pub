b036e775e8b61b6780f7d18642abe87e540ed963
scipima <marco.scipioni05@gmail.com>
2024-05-06 14:40:06 +0200

    unnest and repair cols names

--- source_scripts_r/process_list_longdf.R
+++ source_scripts_r/process_list_longdf.R
@@ -3,22 +3,22 @@
 ###--------------------------------------------------------------------------###
 
 # decided_on_a_realization_of
-process_decided_on_a_realization_of <- function(votes_raw = vote_list_tmp[["MTG-PL-2024-02-27"]]) {
+process_decided_on_a_realization_of <- function(votes_raw = resp_list[["MTG-PL-2024-02-27"]]) {
   if("decided_on_a_realization_of" %in% names(votes_raw) ) {
     decided_on_a_realization_of <- votes_raw |>
       dplyr::select(activity_id, decided_on_a_realization_of) |>
       dplyr::distinct() |> # DEFENSIVE: there may be duplicate rows
-      tidyr::unnest(decided_on_a_realization_of) 
+      tidyr::unnest(decided_on_a_realization_of)
     # return data.frame
     return(decided_on_a_realization_of) } }
 
 
 # was_motivated_by
-process_was_motivated_by <- function(votes_raw = vote_list_tmp[["MTG-PL-2024-02-27"]]) {
+process_was_motivated_by <- function(votes_raw = resp_list[["MTG-PL-2024-02-27"]]) {
   if("was_motivated_by" %in% names(votes_raw) ) {
     was_motivated_by <- votes_raw |>
       dplyr::select(activity_id, was_motivated_by) |>
       dplyr::distinct() |> # DEFENSIVE: there may be duplicate rows
-      tidyr::unnest(was_motivated_by)
+      tidyr::unnest(was_motivated_by, names_sep = "_")
     # return data.frame
     return(was_motivated_by) } }
